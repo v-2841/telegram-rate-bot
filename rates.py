217d70caa229e1de0a4964d8ce6d238d27efe6b9
Vitaliy Pavlov <v-2841@yandex.ru>
2023-09-28 17:37:05 +0000

    fix session

--- rates.py
+++ rates.py
@@ -27,27 +27,26 @@ url_template = ('https://cdn.jsdelivr.net/gh/fawazahmed0/'
                 + 'currency-api@1/latest/currencies/{}/{}.json')
 
 
-async def get_rate(pair):
-    async with ClientSession() as session:
-        async with session.get(url=url_template.format(*pair)) as response:
-            data = await response.json()
-            return (f'{translation[pair[0]]} -> {translation[pair[1]]}: '
-                    + f'{data[pair[1]]}')
+async def get_rate(client, pair):
+    async with client.get(url=url_template.format(*pair)) as response:
+        data = await response.json()
+        return (f'{translation[pair[0]]} -> {translation[pair[1]]}: '
+                + f'{data[pair[1]]}')
 
 
-async def get_date():
-    async with ClientSession() as session:
-        url = url_template.format(*pairs[0])
-        async with session.get(url) as response:
-            data = await response.json()
-            return f'Дата: {data["date"]}'
+async def get_date(client):
+    url = url_template.format(*pairs[0])
+    async with client.get(url) as response:
+        data = await response.json()
+        return f'Дата: {data["date"]}'
 
 
 async def rates(update, context):
     chat = update.effective_chat
-    tasks = [asyncio.create_task(get_date())]
+    tasks = [asyncio.create_task(get_date(context.bot_data['client']))]
     for pair in pairs:
-        tasks.append(asyncio.create_task(get_rate(pair)))
+        tasks.append(asyncio.create_task(
+            get_rate(context.bot_data['client'], pair)))
     rates = await asyncio.gather(*tasks)
     rates_text = '\n'.join(rates)
     await context.bot.send_message(
@@ -68,9 +67,18 @@ async def wake_up(update, context):
     )
 
 
+async def post_init(application: Application) -> None:
+    application.bot_data['client'] = ClientSession()
+
+
+async def post_shutdown(application: Application) -> None:
+    await application.bot_data['client'].close()
+
+
 if __name__ == '__main__':
     application = Application.builder().token(
-        os.getenv('TELEGRAM_TOKEN', 'token')).build()
+        os.getenv('TELEGRAM_TOKEN', 'token')).post_init(
+            post_init).post_shutdown(post_shutdown).build()
     application.add_handler(CommandHandler('start', wake_up))
     application.add_handler(MessageHandler(
         filters.Regex('^Курс на сегодня$'), rates))
